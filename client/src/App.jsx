d0773b6540c27c07beb00125b27f50ae08ead3b4
KODIYARASU01 <kodiyarasu02gmail.com>
2024-04-27 08:56:36 +0530

    Digitalcard7 client side finished

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import NewCardDesign7 from './Component/NewCardDesign7';
+
+const App = () => {
+  return (
+    <div>
+<NewCardDesign7/>
+    </div>
+  )
+}
+
+export default App;
